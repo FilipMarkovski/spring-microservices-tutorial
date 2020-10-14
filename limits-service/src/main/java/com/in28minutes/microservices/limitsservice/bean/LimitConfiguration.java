fdc21cc379bc13aebcaa054dedb0c48c89fd55bd
Filip Markovski <filipmarkovski@gmail.com>
2020-10-14 22:53:56 +0200

    Created Spring Cloud Config Server and Limits Service

---
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,24 @@
+package com.in28minutes.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+
+	private int maximum;
+	private int minimum;
+
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	
+	public LimitConfiguration() {
+		super();
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+}

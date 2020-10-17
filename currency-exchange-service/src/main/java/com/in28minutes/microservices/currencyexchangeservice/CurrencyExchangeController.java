bb1c3310017653531155cb21eee040684f832e18
Filip Markovski <filipmarkovski@gmail.com>
2020-10-17 18:19:35 +0200

    Finished tutorial

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.in28minutes.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +19,16 @@ public class CurrencyExchangeController {
 	@Autowired
 	ExchangeValueRepository repository;
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		
 		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
 		
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		
+		logger.info("{}", exchangeValue);
 		return exchangeValue;
 		
 	}
